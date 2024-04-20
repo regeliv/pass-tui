f6a6c48e629f10eb79a9eafa8d52c2cb3271b366
regeliv <124085151+regeliv@users.noreply.github.com>
2024-04-20 15:37:00 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+def passstore_exists():
+    return True
+
+def decrypt():
+    print("Decrypting")
+
+def open_pass_store():
+    decrypt()
+    pass
+
+def create_pass_store():
+    pass
+
+def load_theme():
+    print("Loading autumn theme")
+
+def start_ui():
+    load_theme()
+    print("Opening Notcurses.")
+
+def read_config():
+    # finds pass store if it exists
+    # reads config file if it exists
+    print("Finding passstore")
+    print("Reading user config")
+
+if __name__ == "__main__":
+    read_config()
+    start_ui()
+    if passstore_exists():
+        open_pass_store()
+    else:
+        create_pass_store()
+
+    
+
+
+
+
